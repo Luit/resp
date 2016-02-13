95ea7d8e9303c9949938e64d77a7cc89d51706a5
Luit van Drongelen <luit@luit.eu>
2016-02-13 14:56:29 +0100

    move RESPError into errors.go

---
+++ errors.go
@@ -0,0 +1,24 @@
+package resp
+
+// RESPError is an error value that can be directly used as a RESP Error value
+// to a client
+type RESPError interface {
+	RESP() []byte
+	error
+}
+
+type respError string
+
+func (e respError) Error() string {
+	return string(e)
+}
+
+func (e respError) RESP() []byte {
+	return append(append([]byte{'-'}, string(e)...), "\r\n"...)
+}
+
+const (
+	errUnbalancedQuotes       respError = "ERR Protocol error: unbalanced quotes in request"
+	errInvalidMultibulkLength respError = "ERR Protocol error: invalid multibulk length"
+	errInvalidBulkLength      respError = "ERR Protocol error: invalid bulk length"
+)
--- resp.go
+++ resp.go
@@ -6,29 +6,6 @@ import (
 	"errors"
 )
 
-// RESPError is an error value that can be directly used as a RESP Error value
-// to a client
-type RESPError interface {
-	RESP() []byte
-	error
-}
-
-type respError string
-
-func (e respError) Error() string {
-	return string(e)
-}
-
-func (e respError) RESP() []byte {
-	return append(append([]byte{'-'}, string(e)...), "\r\n"...)
-}
-
-const (
-	errUnbalancedQuotes       respError = "ERR Protocol error: unbalanced quotes in request"
-	errInvalidMultibulkLength respError = "ERR Protocol error: invalid multibulk length"
-	errInvalidBulkLength      respError = "ERR Protocol error: invalid bulk length"
-)
-
 var errIncompleteCommand = errors.New("incomplete command")
 
 // parseCommand parses a Redis Serialisation Protocol Command, returning the
